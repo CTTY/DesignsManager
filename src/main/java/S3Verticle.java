949580b51858b13a095633cca661e3a78a562300
Yexiang Chang <ychang@mbp-ychang.tracelinkcorp.com>
2019-07-12 16:41:51 -0400

    S3 class

--- src/main/java/S3Verticle.java
+++ src/main/java/S3Verticle.java
@@ -1,4 +1,62 @@
-package PACKAGE_NAME;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.nio.file.Path;
 
 public class S3Verticle {
+    private S3Client s3;
+
+    private final String fileBucketName = "Designs";
+    private final String descriptionBucketName = "Descriptions";
+
+    S3Verticle(){
+        /*Intializing S3*/
+        Region region = Region.US_EAST_1;
+        s3 = S3Client.builder().region(region).build();
+    }
+
+
+    /** Put design on the website*/
+    private void putDesign(Path path, String description){
+        s3.putObject(PutObjectRequest.builder()
+                        .bucket(fileBucketName)
+                        .key(path.getFileName().toString())
+                        .build()
+                , path);
+
+        File tempFile = new File("tempFile");
+        if(tempFile.exists()){
+            tempFile.delete();
+            try{
+                tempFile.createNewFile();
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+        }
+        try{
+            FileWriter fw = new FileWriter(tempFile);
+            fw.write(description);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        s3.putObject(PutObjectRequest.builder()
+                        .bucket(descriptionBucketName)
+                        .key(path.getFileName().toString())
+                        .build(), tempFile.toPath());
+
+    }
+
+    /** Delete design on the website*/
+    private void deleteDesign(String fileName){
+
+    }
+
+    /** Replace design on the website*/
+    private void replaceDesign(Path path, String description){
+
+    }
 }
